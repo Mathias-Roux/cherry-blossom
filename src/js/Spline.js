ab35c1a71ade9c6467467e9e59c393640d5459dc
Mathias Roux <mathiasroux.pro@gmail.com>
2022-05-23 23:26:09 +0200

    gallerie wip

---
+++ src/js/Spline.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const splineScene = document.getElementById('canvas3d');
+const app = new Application(splineScene);
+app.load('https://prod.spline.design/OtYEBTIMtsrepoZW/scene.splinecode');
---
+++ src/js/date.js
@@ -0,0 +1,3 @@
+const date = new Date()
+
+document.querySelector('.date > span').innerHTML = date.toDateString()
