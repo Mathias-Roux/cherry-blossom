cce130b071f9f9428f2eb83aa78f5ff3a4efe3de
Mathias Roux <mathiasroux.pro@gmail.com>
2022-05-24 11:46:37 +0200

    horizontal gallerie wip

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 import './bg-sakura.js'
 import './date.js'
 import './Spline.js'
+import './gsap_animations.js'
---
+++ src/js/gsap_animations.js
@@ -0,0 +1,20 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.to(".gallerie", {
+  xPercent: -100, 
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".gallerie",
+    start: "top top",
+    end: () => innerWidth * 5,
+    scrub: true,
+    pin: true,
+    markers: true,
+    invalidateOnRefresh: true,
+    anticipatePin: 1
+  }
+});
